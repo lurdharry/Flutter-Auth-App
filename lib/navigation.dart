a73c386ccb34fa9df3c4d94a2552281e3344bb52
lurdharry <azeezhammed57@gmail.com>
2020-10-28 14:11:17 +0100

    added navigation file

--- lib/navigation.dart
+++ lib/navigation.dart
@@ -1,4 +1,8 @@
+import 'dart:js';
+
+import 'package:TestApp/screens/Home/homeScreen.dart';
 import 'package:TestApp/screens/Login/login_screen.dart';
+import 'package:TestApp/screens/signUpscreen/signupscreen.dart';
 import 'package:flutter/widgets.dart';
 
 // import 'package:shop_app/screens/sign_in/sign_in_screen.dart';
@@ -8,5 +12,6 @@ import 'package:flutter/widgets.dart';
 // All our routes will be available here
 final Map<String, WidgetBuilder> navigation  = {
   LoginScreen.routeName: (context) => LoginScreen(),
- 
+  SignUpScreen.routeName: (context) => SignUpScreen(),
+  HomeScreen.routeName:(context)=>HomeScreen(),
 };
