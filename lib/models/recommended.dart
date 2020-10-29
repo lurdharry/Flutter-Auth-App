29a1d57a3ed4ec0a63306b4b3e90aeb81979856e
lurdharry <azeezhammed57@gmail.com>
2020-10-29 21:57:31 +0100

    Added recommended sample  data

---
+++ lib/models/recommended.dart
@@ -0,0 +1,26 @@
+class RecoItem {
+  final int key;
+  final String image;
+
+  RecoItem({this.key, this.image});
+  
+}
+
+List<RecoItem> dummyRecoData =[
+  new RecoItem(
+    key :1,
+    image: "assets/images/1.png",
+  ),
+  new RecoItem(
+    key :2,
+    image: "assets/images/2.png",
+  ),
+  new RecoItem(
+    key :3,
+    image: "assets/images/3.png",
+  ),
+  new RecoItem(
+    key :3,
+    image: "assets/images/4.png",
+  ),
+];
